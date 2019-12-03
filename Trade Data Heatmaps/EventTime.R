fa7362640b12d3caaaf84628b08e0fee09699f4a
Patrick Chang <patrickchang@Marks-MacBook-Pro.local>
2019-12-03 14:57:29 +0200

    EventTime Heatmaps updated

--- Trade Data Heatmaps/EventTime.R
+++ Trade Data Heatmaps/EventTime.R
@@ -2,7 +2,7 @@
 # Script file to plot the Event Line Up data heatmaps
 
 #--------------------------------------------------------------------------
-# Lining Up Events
+# Lining Up Events (Most liquid)
 
 library(reshape2)
 library(tidyverse)
@@ -142,7 +142,143 @@ ggplot(data = melted_corr, aes(Var1,Var2 , fill = value))+
 
 
 
+#--------------------------------------------------------------------------
+# Lining Up Events (Least liquid)
 
+library(reshape2)
+library(tidyverse)
+library("RColorBrewer")
+data = readRDS("Computed Correlation/MMHYTimBucket48LL.RData")
+
+tickers = c("BTI","NPN","AGL","MNP*","SOL","SBK","NED","ABG","SHP","FSR") 
+MM48VolumeBuckets = data[[1]]
+HY48VolumeBuckets = data[[2]] 
+data = readRDS("Computed Correlation/MMHYTimBucket480LL.RData")
+MM480VolumeBuckets = data[[1]]
+HY480VolumeBuckets = data[[2]]
+
+#--------------------------------------------------------------------------
+## Malavian Mancino 
+# 48 Buckets
+stock_correlations = MM48VolumeBuckets$Correlation
+colnames(stock_correlations) = tickers
+rownames(stock_correlations) = tickers
+melted_corr = melt(stock_correlations)
+color <- colorRampPalette(c('red','orange',"yellow",'springgreen', 'royalblue'))(sqrt(NROW((melted_corr))))%>%rev()
+round(mean(abs(stock_correlations[lower.tri(stock_correlations)])), 4)
+round(sd(abs(stock_correlations[lower.tri(stock_correlations)])), 4)
+
+
+ggplot(data = melted_corr, aes(Var1,Var2 , fill = value))+
+  geom_tile()+
+  scale_fill_gradientn(colours = color,limit=c(-1,max(stock_correlations)),name=TeX("$\\rho$")) +
+  theme_bw()+ 
+  labs(title = "(a) MM 48 Least Liquid")+
+  annotate("text", x = 7, y = 3, label = TeX("$\\bar{| \\rho_{ij} | } = 0.0034 \\pm 0.005"), size = 6) + 
+  theme(legend.key.size = unit(1.7, "cm"),
+        legend.key.width = unit(0.5,"cm"),
+        legend.title = element_text(size = 13, face = "bold"),
+        panel.border = element_rect(),
+        panel.grid.major= element_blank(),
+        panel.grid.minor= element_blank(),
+        panel.background = element_blank(),
+        panel.grid = element_blank(),
+        axis.title = element_blank(),
+        axis.text.y = element_text(face="bold", size=13),
+        axis.text.x = element_text(face="bold", size=13, angle = 90),
+        plot.title = element_text(face="bold", colour="black", size=15,hjust=0.5,vjust=0))+coord_fixed()
+
+
+# 480 Buckets
+stock_correlations = MM480VolumeBuckets$Correlation
+colnames(stock_correlations) = tickers
+rownames(stock_correlations) = tickers
+melted_corr = melt(stock_correlations)
+color <- colorRampPalette(c('red','orange',"yellow",'springgreen', 'royalblue'))(sqrt(NROW((melted_corr))))%>%rev()
+round(mean(abs(stock_correlations[lower.tri(stock_correlations)])), 4)
+round(sd(abs(stock_correlations[lower.tri(stock_correlations)])), 4)
+
+
+ggplot(data = melted_corr, aes(Var1,Var2 , fill = value))+
+  geom_tile()+
+  scale_fill_gradientn(colours = color,limit=c(-1,max(stock_correlations)),name=TeX("$\\rho$")) +
+  theme_bw()+ 
+  labs(title = "(b) MM 480 Least Liquid")+
+  annotate("text", x = 7, y = 3, label = TeX("$\\bar{| \\rho_{ij} | } = 0.0103 \\pm 0.0084"), size = 6) + 
+  theme(legend.key.size = unit(1.7, "cm"),
+        legend.key.width = unit(0.5,"cm"),
+        legend.title = element_text(size = 13, face = "bold"),
+        panel.border = element_rect(),
+        panel.grid.major= element_blank(),
+        panel.grid.minor= element_blank(),
+        panel.background = element_blank(),
+        panel.grid = element_blank(),
+        axis.title = element_blank(),
+        axis.text.y = element_text(face="bold", size=13),
+        axis.text.x = element_text(face="bold", size=13, angle = 90),
+        plot.title = element_text(face="bold", colour="black", size=15,hjust=0.5,vjust=0))+coord_fixed()
+
+
+
+## Hayashi Yoshida
+# 48 Buckets
+stock_correlations = HY48VolumeBuckets$Correlation
+colnames(stock_correlations) = tickers
+rownames(stock_correlations) = tickers
+melted_corr = melt(stock_correlations)
+color <- colorRampPalette(c('red','orange',"yellow",'springgreen', 'royalblue'))(sqrt(NROW((melted_corr))))%>%rev()
+round(mean(abs(stock_correlations[lower.tri(stock_correlations)])), 4)
+round(sd(abs(stock_correlations[lower.tri(stock_correlations)])), 4)
+
+
+ggplot(data = melted_corr, aes(Var1,Var2 , fill = value))+
+  geom_tile()+
+  scale_fill_gradientn(colours = color,limit=c(-1,max(stock_correlations)),name=TeX("$\\rho$")) +
+  theme_bw()+ 
+  labs(title = "(c) HY 48 Least Liquid")+
+  annotate("text", x = 7, y = 3, label = TeX("$\\bar{| \\rho_{ij} | } = 0.2416 \\pm 0.1994"), size = 6) + 
+  theme(legend.key.size = unit(1.7, "cm"),
+        legend.key.width = unit(0.5,"cm"),
+        legend.title = element_text(size = 13, face = "bold"),
+        panel.border = element_rect(),
+        panel.grid.major= element_blank(),
+        panel.grid.minor= element_blank(),
+        panel.background = element_blank(),
+        panel.grid = element_blank(),
+        axis.title = element_blank(),
+        axis.text.y = element_text(face="bold", size=13),
+        axis.text.x = element_text(face="bold", size=13, angle = 90),
+        plot.title = element_text(face="bold", colour="black", size=15,hjust=0.5,vjust=0))+coord_fixed()
+
+
+# 480 Buckets
+stock_correlations = HY480VolumeBuckets$Correlation
+colnames(stock_correlations) = tickers
+rownames(stock_correlations) = tickers
+melted_corr = melt(stock_correlations)
+color <- colorRampPalette(c('red','orange',"yellow",'springgreen', 'royalblue'))(sqrt(NROW((melted_corr))))%>%rev()
+round(mean(abs(stock_correlations[lower.tri(stock_correlations)])), 4)
+round(sd(abs(stock_correlations[lower.tri(stock_correlations)])), 4)
+
+
+ggplot(data = melted_corr, aes(Var1,Var2 , fill = value))+
+  geom_tile()+
+  scale_fill_gradientn(colours = color,limit=c(-1,max(stock_correlations)),name=TeX("$\\rho$")) +
+  theme_bw()+ 
+  labs(title = "(d) HY 480 Least Liquid")+
+  annotate("text", x = 7, y = 3, label = TeX("$\\bar{| \\rho_{ij} | } = 0.1284 \\pm 0.0929"), size = 6) + 
+  theme(legend.key.size = unit(1.7, "cm"),
+        legend.key.width = unit(0.5,"cm"),
+        legend.title = element_text(size = 13, face = "bold"),
+        panel.border = element_rect(),
+        panel.grid.major= element_blank(),
+        panel.grid.minor= element_blank(),
+        panel.background = element_blank(),
+        panel.grid = element_blank(),
+        axis.title = element_blank(),
+        axis.text.y = element_text(face="bold", size=13),
+        axis.text.x = element_text(face="bold", size=13, angle = 90),
+        plot.title = element_text(face="bold", colour="black", size=15,hjust=0.5,vjust=0))+coord_fixed()
 
 
 
